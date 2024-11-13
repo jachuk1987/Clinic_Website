800664302e96c9b143512a4ba83ded85d1b8b439
jeswanth <jachu.k1212@gmail.com>
2024-11-13 07:35:04 +0530

    Clinic_Website

--- clinic_website/src/App.js
+++ clinic_website/src/App.js
@@ -1,33 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import Spinner from './Components/Spinner';
-import HeaderTopBar from './Components/Topbar';
+
+import Topsection from './Components/Topsection';
 import Navbar from './Components/Navbar';
 import Header from './Components/Header';
-import AboutUs from './Components/About';
-import Services from './Components/Service';
-import Features from './Components/Feature';
-import Doctors from './Components/Team';
+import About from './Components/About';
+import Services from './Components/Services';
+import Future from './Components/Future';
+import Team from './Components/Team';
 import Appointment from './Components/Appointment';
-import Testimonials from './Components/Testimonial';
+import Testsection from './Components/Testsection';
 import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Spinner/>
-     
-      <HeaderTopBar/>
-      <Navbar/>
-      <Header/>
-      <AboutUs/>
-      <Services/>
-      <Features/>
-      <Doctors/>
-      <Appointment/>
-      <Testimonials/>
-      <Footer/>
-
+      <Topsection />
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Future />
+      <Team />
+      <Appointment />
+      <About/>
+      <Testsection />
+      <Footer />
     </div>
   );
 }
--- clinic_website/src/Components/Appointment.jsx
+++ clinic_website/src/Components/Appointment.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Appointment extends Component {
     render() {
@@ -11,8 +11,8 @@ class Appointment extends Component {
                             <h1 className="mb-4">Make An Appointment To Visit Our Doctor</h1>
                             <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
                             <div className="bg-light rounded d-flex align-items-center p-5 mb-4">
-                                <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style={{ width: '55px', height: '55px' }}>
-                                    <i className="fa fa-phone-alt text-primary"></i>
+                                <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style={{ width: 55, height: 55 }}>
+                                    <i className="fa fa-phone-alt text-primary" />
                                 </div>
                                 <div className="ms-4">
                                     <p className="mb-2">Call Us Now</p>
@@ -20,8 +20,8 @@ class Appointment extends Component {
                                 </div>
                             </div>
                             <div className="bg-light rounded d-flex align-items-center p-5">
-                                <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style={{ width: '55px', height: '55px' }}>
-                                    <i className="fa fa-envelope-open text-primary"></i>
+                                <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style={{ width: 55, height: 55 }}>
+                                    <i className="fa fa-envelope-open text-primary" />
                                 </div>
                                 <div className="ms-4">
                                     <p className="mb-2">Mail Us Now</p>
@@ -34,34 +34,34 @@ class Appointment extends Component {
                                 <form>
                                     <div className="row g-3">
                                         <div className="col-12 col-sm-6">
-                                            <input type="text" className="form-control border-0" placeholder="Your Name" style={{ height: '55px' }} />
+                                            <input type="text" className="form-control border-0" placeholder="Your Name" style={{ height: 55 }} />
                                         </div>
                                         <div className="col-12 col-sm-6">
-                                            <input type="email" className="form-control border-0" placeholder="Your Email" style={{ height: '55px' }} />
+                                            <input type="email" className="form-control border-0" placeholder="Your Email" style={{ height: 55 }} />
                                         </div>
                                         <div className="col-12 col-sm-6">
-                                            <input type="text" className="form-control border-0" placeholder="Your Mobile" style={{ height: '55px' }} />
+                                            <input type="text" className="form-control border-0" placeholder="Your Mobile" style={{ height: 55 }} />
                                         </div>
                                         <div className="col-12 col-sm-6">
-                                            <select className="form-select border-0" style={{ height: '55px' }}>
+                                            <select className="form-select border-0" style={{ height: 55 }}>
                                                 <option selected>Choose Doctor</option>
-                                                <option value="1">Doctor 1</option>
-                                                <option value="2">Doctor 2</option>
-                                                <option value="3">Doctor 3</option>
+                                                <option value={1}>Doctor 1</option>
+                                                <option value={2}>Doctor 2</option>
+                                                <option value={3}>Doctor 3</option>
                                             </select>
                                         </div>
                                         <div className="col-12 col-sm-6">
                                             <div className="date" id="date" data-target-input="nearest">
-                                                <input type="text" className="form-control border-0 datetimepicker-input" placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style={{ height: '55px' }} />
+                                                <input type="text" className="form-control border-0 datetimepicker-input" placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style={{ height: 55 }} />
                                             </div>
                                         </div>
                                         <div className="col-12 col-sm-6">
                                             <div className="time" id="time" data-target-input="nearest">
-                                                <input type="text" className="form-control border-0 datetimepicker-input" placeholder="Choose Date" data-target="#time" data-toggle="datetimepicker" style={{ height: '55px' }} />
+                                                <input type="text" className="form-control border-0 datetimepicker-input" placeholder="Choose Date" data-target="#time" data-toggle="datetimepicker" style={{ height: 55 }} />
                                             </div>
                                         </div>
                                         <div className="col-12">
-                                            <textarea className="form-control border-0" rows="5" placeholder="Describe your problem"></textarea>
+                                            <textarea className="form-control border-0" rows={5} placeholder="Describe your problem" defaultValue={""} />
                                         </div>
                                         <div className="col-12">
                                             <button className="btn btn-primary w-100 py-3" type="submit">Book Appointment</button>
@@ -73,8 +73,9 @@ class Appointment extends Component {
                     </div>
                 </div>
             </div>
-        );
+
+        )
     }
 }
 
-export default Appointment;
+export default Appointment
